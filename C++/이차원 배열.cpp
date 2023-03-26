cda8a2ec2cebc9ef6ff7c6573db35800f0825276
류성 <rocsa001@naver.com>
2023-03-26 16:19:54 +0900

    first commit

---
+++ C++/이차원 배열.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class ThreeMattices{
+    int a[3][5]={{5,10,2,7,5},{4,6,2,2,9},{1,9,2,8,4}};
+    int b[3][5]={{5,2,7,4,5},{10,6,9,2,3,},{2,6,4,7,1}};
+    int c[3][5];
+    public:
+
+}
+
+int main()
+{
+    
+}
